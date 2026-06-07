2ecf4a7dcf7acbecc5f52c1a1bf2c8c35e9de61d
corridaacy-ux <corridaacy@xinguara.pa.gov.br>
2026-06-07 14:07:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const LINK_GOOGLE_FORMS = "https://forms.gle/wceRLE6F1drY1uch6";
 const DATA_EVENTO = "2026-08-08T06:30:00-03:00";
 
 document.querySelectorAll('a[href="https://forms.gle/wceRLE6F1drY1uch6"]').forEach(link => {
-  link.href = LINK_GOOGLE_FORMS;
+  link.href = https://forms.gle/wceRLE6F1drY1uch6;
 });
 
 function atualizarContador() {
